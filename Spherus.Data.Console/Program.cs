fd5f9537e39f9d494f5ab5932f35c21d605e52b3
rostislavrotaru <rostislav.rotaru@roluan.com>
2015-11-22 11:26:03 +0200

    Working on Structure

--- Spherus.Data.Console/Program.cs
+++ Spherus.Data.Console/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Spherus.Data.Console
+﻿namespace Spherus.Data.Console
 {
     public class Program
     {
